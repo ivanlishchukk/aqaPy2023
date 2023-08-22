5f2c125a2d9005492aea5422a34fa255085e7e60
Ivan Lishchuk <i.lishchuk@icloud.com>
2023-08-22 21:11:52 +0300

    HW_14

--- hw_14/csv_test/test_csv.py
+++ hw_14/csv_test/test_csv.py
@@ -5,10 +5,14 @@ class TestCSV:
 
     def test_add_row(self):
         csv_file = CSVManipulation()
+        len_before = csv_file.csv_len('example.csv')
         csv_file.add_row('example.csv', row_data='Ivan, Lishchuk, age, Male, salary')
-        assert csv_file.csv_len('example.csv') == 5
+        len_after = csv_file.csv_len('example.csv')
+        assert len_after - len_before == 1
 
     def test_delete_row(self):
         csv_file = CSVManipulation()
-        csv_file.delete_row('example.csv', row_index=3)
-        assert csv_file.csv_len('example.csv') == 4
+        len_before = csv_file.csv_len('example.csv')
+        csv_file.delete_row('example.csv', row_index=len_before-2)
+        len_after = csv_file.csv_len('example.csv')
+        assert len_before - len_after == 1
